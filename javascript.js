72fd9f99e8fd75b8cd72817d14d2de7a3e1a54df
Steven Nguyen <105184434+SdbnCode@users.noreply.github.com>
2023-11-10 18:33:51 -0500

    wrote playRound function but still has bugs

--- javascript.js
+++ javascript.js
@@ -1,12 +1,33 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3);
     if (choice === 1){
-        return "Rock";
+        return "rock";
     }else if(choice === 2){
-        return "Paper";
+        return "paper";
     }else{
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    computerSelection.toLowerCase();
+if(playerSelection === computerSelection){
+        return "tie";
+    }else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You Win! Rock beats Paper";
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock";
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beats Paper";
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beats Scissors";
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You Win! Scissors beats Paper";
+    }else {
+        return "You Lose! Rock beats Paper";
+    }
+}
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
